da776c5a57a069e4d752e958e1cc35ec66aa78f3
Adam <adam.vichik@gmail.com>
2020-01-10 12:38:14 +0200

    Initial commit

---
+++ src/test/java/frc/robot/subsystems/ExampleSubsystemTest.java
@@ -0,0 +1,22 @@
+package frc.robot.subsystems;
+
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+
+public class ExampleSubsystemTest {
+
+    ExampleSubsystem test_obj = new ExampleSubsystem();
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void periodic() {
+    }
+}
